96232862edf3834a3494c176bdae760bc08a3943
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-02-11 20:41:13 +0100

    first commit

---
+++ react-app/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+
+    }
+  }
+
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+}
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
+
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+
+reportWebVitals();

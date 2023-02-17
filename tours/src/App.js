25ea14d452bea1e684e771022c469120a53ff6b8
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-02-17 02:52:50 +0100

    [Tours] css styling started

---
+++ tours/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id="container">
+      <h1>Tours App</h1>
+      <div id="tours">
+        <div class="tour">
+          <img src="https://images.pexels.com/photos/4823473/pexels-photo-4823473.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          <div class="tour-details">
+            <div class="tour-details-header">
+              <h4>Best Of Paris In 7 Days Tour</h4>
+              <span class="price">1,995$</span>
+            </div>
+            <p class="description">
+              LKDSAK;LSDKA;DKSDSAKDSDJSADOIWUOEQJUWOESAOIJDSAIOJDSAIODJSAOJASIJDSAOIJSIODISAIDJSAODSAJAODSIDSAOIDSAIODJSA
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

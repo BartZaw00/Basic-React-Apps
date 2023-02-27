e6290fb5e533b7baa24ad1d539c37fdff058242a
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-02-27 05:26:48 +0100

    [Grocery] adding and clearing all list elements done

--- grocery/src/App.jsx
+++ grocery/src/App.jsx
@@ -1,22 +1,63 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [clearBtn, setClearBtn] = useState(false);
+  const [listElement, setListElement] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (listElement != "") {
+      setClearBtn(true);
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: listElement,
+      };
+      setList([...list, newItem]);
+      setListElement("");
+    }
+  };
+
+  const handleChange = (e) => {
+    setListElement(e.target.value);
+  };
 
   return (
     <div className="container">
       <h1>Grocery Bud</h1>
       <div className="container-input">
-        <form className="form-element">
-          <input type="text" placeholder="e.g. Milk" />
-          <button type="submit">Submit</button>
+        <form className="form-element" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="e.g. Milk"
+            value={listElement}
+            onChange={handleChange}
+          />
+          <button type="submit" className="list-button">
+            Submit
+          </button>
         </form>
-        <ul></ul>
-        <div className="clear-button display-none">
-          Clear Items
-        </div>
+        <ul>
+          {list.map((item) => {
+            return (
+              <li key={item.id}>
+                <span>{item.title}</span>
+              </li>
+            );
+          })}
+        </ul>
+        {clearBtn ? (
+          <div
+            className="clear-button"
+            onClick={() => {
+              setList([]);
+              setClearBtn(!clearBtn);
+            }}
+          >
+            Clear Items
+          </div>
+        ) : null}
       </div>
     </div>
   );
